f16935d66de7e6c2c6322f209c6980d5223dac6b
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-09-29 04:53:14 +0200

    Update text_summarization_app.py

--- text_summarization_app.py
+++ text_summarization_app.py
@@ -14,13 +14,13 @@ st.title("Text Summarizer")
 #OPENAI_KEY=st.text_area("Copy & Paste your OPENAI_KEY", key="OPENAI_KEY")
 #OPENAI_KEY=os.getenv('OPENAI_KEY')
 text_input = st.empty() 
-if st.button(“Clear text input”): 
- text_input.text_area(“Copy & Paste your text data”,height=320, value=‘’)
+if st.button("Clear text input"): 
+ text_input.text_area("Copy & Paste your text data",height=320, value='')
 
-OPENAI_KEY = st.text_input(“Copy & Paste your OPENAI_KEY”, type=‘password’) 
-os.environ[“OPENAI_API_KEY”] = OPENAI_KEY
+OPENAI_KEY = st.text_input("Copy & Paste your OPENAI_KEY", type='password') 
+os.environ["OPENAI_API_KEY"] = OPENAI_KEY
 #text=st.text_area("Copy & Paste your text data",height=320, key="text")
-text = text_input.text_area(“Copy & Paste your text data”,height=320)
+text = text_input.text_area("Copy & Paste your text data",height=320)
 
 tokens = st.slider('Insert a number', max_value=1000)
 st.write('Max number of tokens: ', tokens)
@@ -44,14 +44,14 @@ if submit:
             response = chain.run(docs)
             
     #st.text_area("Output data",value=response, height=320)
-    text_output.text_area(“Output data”,value=response, height=320)
+    text_output.text_area("Output data",value=response, height=320)
 #def clear_text():
 #    st.session_state['submit'] = ""
     
 #st.button("clear text output", on_click=clear_text)
 text_output = st.empty()
-if st.button(“Clear text output”): 
- text_output.text_area(“Output data”,value=‘’, height=320)
+if st.button("Clear text output"): 
+ text_output.text_area("Output data",value='', height=320)
 
     
     
